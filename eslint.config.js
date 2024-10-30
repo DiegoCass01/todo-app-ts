ce329709f495ac67ecb4049f5e36b6951d6ae975
DiegoCass01 <diego.castillo170901@gmail.com>
2024-10-29 20:50:59 -0600

    initial commit

---
+++ eslint.config.js
@@ -0,0 +1,25 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+import pluginReact from "eslint-plugin-react";
+
+export default [
+  { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"] },
+  { languageOptions: { globals: globals.browser } },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  pluginReact.configs.flat.recommended,
+  {
+    rules: {
+      "reac/prop-types": "off",
+      "react/react-in-jsx-scope": "off",
+    },
+  },
+  {
+    parserOptions: {
+      ecmaVersion: "latest",
+      sourceType: "module",
+      project: "./tsconfig.json",
+    },
+  },
+];
